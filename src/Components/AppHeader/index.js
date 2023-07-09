3360a0751b42b7c5b7323c00f7ceb8da4fb7a4dd
hemanshu1390 <ykasar138@gmail.com>
2023-07-09 17:44:49 +0530

    Dashboard

---
+++ src/Components/AppHeader/index.js
@@ -0,0 +1,81 @@
+import { Badge, Drawer, Image, List, Space, Typography,Button } from "antd";
+import { useEffect, useState } from "react";
+import { getComments, getOrders } from "../../API";
+import { createFromIconfontCN } from '@ant-design/icons';
+
+const IconFont = createFromIconfontCN({
+  scriptUrl: '//at.alicdn.com/t/font_8d5l8fzk5b87iudi.js',
+});
+function AppHeader() {
+  const [comments, setComments] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [commentsOpen, setCommentsOpen] = useState(false);
+  const [notificationsOpen, setNotificationsOpen] = useState(false);
+
+  useEffect(() => {
+    getComments().then((res) => {
+      setComments(res.comments);
+    });
+    getOrders().then((res) => {
+      setOrders(res.products);
+    });
+  }, []);
+
+  return (
+    <div className="AppHeader">
+      <Image
+        width={40}
+       
+      ></Image>
+      <Typography.Title>Admin Dashboard</Typography.Title>
+      <Space>
+    
+        <Badge >
+       
+        </Badge>
+      </Space>
+      <Drawer
+        title="Comments"
+        open={commentsOpen}
+        onClose={() => {
+          setCommentsOpen(false);
+        }}
+        maskClosable
+      >
+        <List
+          dataSource={comments}
+          renderItem={(item) => {
+            return <List.Item>{item.body}</List.Item>;
+          }}
+        ></List>
+      </Drawer>
+      <Drawer
+        title="Notifications"
+        open={notificationsOpen}
+        onClose={() => {
+          setNotificationsOpen(false);
+        }}
+        maskClosable
+      >
+        <List
+          dataSource={orders}
+          renderItem={(item) => {
+            return (
+              <List.Item>
+                <Typography.Text strong>{item.title}</Typography.Text> has been
+                ordered!
+              </List.Item>
+            );
+          }}
+        ></List>
+      </Drawer>
+      <div className="pe-12">
+          <Button variant= "gradient" size=""  className=" flex items-center gap-2 bg-red-400 text-white font-bold">
+         <IconFont type="icon-tuichu" size="" className=' '/>
+         <h1>Log Out</h1>
+          </Button>
+        </div>
+    </div>
+  );
+}
+export default AppHeader;
